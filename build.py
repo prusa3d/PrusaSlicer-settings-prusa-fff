2a6ed2c25375badb3007782d0974771acb9679d7
Davide Bortolami <davide.bortolami@thinksmartbox.com>
2025-02-07 17:34:25 +0000

    improve script, comment out rm.ini sections instead of deleting them

--- build.py
+++ build.py
@@ -24,33 +24,32 @@ def find_latest_ini():
 def process_files():
    logging.info('Starting file processing')
    
-   # Create build directory if it doesn't exist
    Path('build').mkdir(exist_ok=True)
    logging.info('Created build directory')
    
-   # Find and read latest ini
    latest_ini = find_latest_ini()
    with open(latest_ini, 'r', encoding='utf-8') as f:
        content = f.read()
    logging.info(f'Read content from {latest_ini}')
 
-   # Process removal files
    smartbox = Path('Smartbox')
    rm_files = list(smartbox.glob('*.rm.ini'))
    logging.info(f'Found {len(rm_files)} removal files')
    
-   # Remove content from rm files
    for rm_file in rm_files:
        with open(rm_file, 'r', encoding='utf-8') as f:
            rm_content = f.read()
-           content = content.replace(rm_content, '')
-           logging.info(f'Removed content from {rm_file}')
+           if rm_content in content:
+               commented_content = '\n'.join(f'# {line}' for line in rm_content.splitlines())
+               header = f'\n{"#" * 80}\n# REMOVED SECTION FROM {rm_file.name}\n{"#" * 80}\n'
+               footer = f'\n{"#" * 80}\n# END REMOVED SECTION FROM {rm_file.name}\n{"#" * 80}\n'
+               replacement = header + commented_content + footer
+               content = content.replace(rm_content, replacement)
+               logging.info(f'Commented out content from {rm_file}')
 
-   # Process addition files
    add_files = list(smartbox.glob('*.add.ini'))
    logging.info(f'Found {len(add_files)} addition files')
    
-   # Add content with padding and headers
    for add_file in add_files:
        with open(add_file, 'r', encoding='utf-8') as f:
            add_content = f.read()
@@ -58,7 +57,6 @@ def process_files():
            content += header + add_content + '\n\n\n\n\n'
            logging.info(f'Added content from {add_file}')
 
-   # Write output
    with open('build/PrusaResearch.ini', 'w', encoding='utf-8') as f:
        f.write(content)
    logging.info('Wrote final output to build/PrusaResearch.ini')
